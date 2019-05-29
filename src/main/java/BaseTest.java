12c77bbc4377efa3c715b1758bc69aa8fac9b647
Neeraj909 <48562614+Neeraj909@users.noreply.github.com>
2019-05-29 17:00:13 +0530

    BaseTest

--- src/main/java/BaseTest.java
+++ src/main/java/BaseTest.java
@@ -1,15 +1,22 @@
+package TestVagrant.basetest;
 
+import java.io.FileInputStream;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
+
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -17,13 +24,22 @@ public class BaseTest {
 
 	private WebDriver webdriver;
 	private WebDriverWait wait;
-	Properties prop=new Properties();
+	private Properties prop;
+	
+	public Properties getProp() {
+		return prop;
+	}
+
+	public void setProp(Properties prop) {
+		this.prop = prop;
+	}
+
 	String property_file_path=System.getProperty("user.dir")+"\\Resources\\";
 	FileInputStream fis;
 
 	@BeforeSuite
 	@Parameters({ "browserName", "url" })
-	public void setup(@Optional("chrome") String browserName, @Optional("http://www.cleartrip.com") String url) {
+	public void setup(@Optional("chrome") String browserName, @Optional("http://www.cleartrip.com") String url) throws IOException {
 
 		switch (browserName.trim().toLowerCase()) {
 		case "firefox":
@@ -48,10 +64,13 @@ public class BaseTest {
 			prefs.put("profile.default_content_setting_values.notifications", 2);
 			options.setExperimentalOption("prefs", prefs);
 			System.setProperty("webdriver.chrome.driver","C:\\Users\\11017\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
- 		        webdriver = new ChromeDriver();
-                        wait = new WebDriverWait(webdriver, 30);
- 		        fis = new FileInputStream(property_file_path+"config.properties");
+ 		    webdriver = new ChromeDriver();
+ 		    wait = new WebDriverWait(webdriver, 30);
+ 		    fis = new FileInputStream(property_file_path+"config.properties");
+ 		    prop=new Properties();
  			prop.load(fis);
+ 		
+ 		
 			//setWebdriver(new ChromeDriver(options));
 			break;
 		}
@@ -65,10 +84,14 @@ public class BaseTest {
 
 		return webdriver;
 	}
-
+	public WebDriverWait getWait() {
+		return wait;
+	}
+	
+	
 	private void setWebdriver(WebDriver webdriver) {
 		this.webdriver = webdriver;
-               wait = new WebDriverWait(webdriver, 30);
+		wait = new WebDriverWait(webdriver, 30);
 	}
 
 	@AfterSuite
